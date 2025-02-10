1cfdc1b6c032d672703c5390449eb36987e0e7bd
Daniil Vasilyev <daniil@tac.build>
2025-02-10 15:56:05 +0300

    added chain check, removed some pools

--- composables/useTonConnect.ts
+++ composables/useTonConnect.ts
@@ -1,13 +1,21 @@
-import type { CHAIN, Account } from '@tonconnect/ui'
-import { THEME, TonConnectUI, toUserFriendlyAddress } from '@tonconnect/ui'
+import type { Account } from '@tonconnect/ui'
+import { CHAIN, THEME, TonConnectUI, toUserFriendlyAddress } from '@tonconnect/ui'
 import { truncate } from '~/utils/string-utils'
+import { SwapStatusModal } from '#components'
+import { useModal } from '~/components/ui/composables/useModal'
 
+const modal = useModal()
 const config = useRuntimeConfig().public
 
 const isLoaded = ref(false)
 const account: Account = reactive({} as Account)
 const walletName = ref('Wallet')
 const address = computed(() => account?.address || '')
+const chain = computed(() => account.chain)
+const friendlyAddress = computed(() => account?.address ? toUserFriendlyAddress(account.address, true) : '')
+const shortAddress = computed(() => friendlyAddress.value ? truncate(friendlyAddress.value) : '')
+const shorterAddress = computed(() => friendlyAddress.value ? truncate(friendlyAddress.value, 3) : '')
+const isConnected = computed(() => Boolean(address.value))
 
 const tonConnectUI = new TonConnectUI({
   manifestUrl: 'https://curve.tac.build/tonconnect-manifest.json',
@@ -28,31 +36,42 @@ tonConnectUI.connectionRestored.then(() => {
   isLoaded.value = true
 })
 tonConnectUI.onStatusChange((walletInfo) => {
+  console.log('onStatusChange', walletInfo)
   if (walletInfo?.account?.address) {
     Object.assign(account, walletInfo.account)
     walletName.value = walletInfo?.name
   }
 })
 
-export const useTonConnect = () => {
-  const friendlyAddress = computed(() => account?.address ? toUserFriendlyAddress(account.address, true) : '')
-  const shortAddress = computed(() => friendlyAddress.value ? truncate(friendlyAddress.value) : '')
-  const shorterAddress = computed(() => friendlyAddress.value ? truncate(friendlyAddress.value, 3) : '')
-  const isConnected = computed(() => Boolean(address.value))
-  const getTonConnectUI = () => {
-    return tonConnectUI
-  }
-  const disconnect = async () => {
-    Object.assign(account, {
-      address: '',
-      chain: '' as CHAIN,
-      walletStateInit: '',
-      publicKey: '',
+const getTonConnectUI = () => {
+  return tonConnectUI
+}
+const disconnect = async () => {
+  Object.assign(account, {
+    address: '',
+    chain: '' as CHAIN,
+    walletStateInit: '',
+    publicKey: '',
+  })
+
+  await tonConnectUI.disconnect()
+}
+
+watch(chain, async (val) => {
+  if (val === CHAIN.MAINNET) {
+    modal.open(SwapStatusModal, {
+      props: {
+        title: 'Mainnet Wallet cannot be connected',
+        text: 'Only Testnet wallets can be used in this app. Change network and try again',
+        status: 'error',
+        buttonLabel: 'Close',
+      },
     })
-
-    await tonConnectUI.disconnect()
+    await disconnect()
   }
+}, { immediate: true })
 
+export const useTonConnect = () => {
   return {
     isLoaded,
     isConnected,
